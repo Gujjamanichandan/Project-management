4afd37c14e4a61129490d55a2da291f33c801d06
Mani Chandan Gujja <gujjamanichandan@gmail.com>
2025-06-19 06:29:52 -0400

    feat: implement project management components and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import{ useState } from "react";
+
+import  NewProject  from "./components/NewProject";
+import ProjectSidebar from "./components/ProjectSidebar";
+import NoProjectSelected from "./components/NoProjrctSelected";
+
+
 function App() {
+  const [projectsState,setProjectsState] = useState({
+    selctedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject(){
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selctedProjectId: null,
+      };
+    });
+
+  }
+  
+  let content;
+
+  if(projectsState.selctedProjectId === null){
+    content = <NewProject />
+  }else if(projectsState.selctedProjectId===undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+ 
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+   <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+   {content}
+    </main>
   );
 }
 
