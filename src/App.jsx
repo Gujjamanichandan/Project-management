1de7e4f62e58ef431b53cf9acf0bc1c2e4b3dd3f
Mani Chandan Gujja <gujjamanichandan@gmail.com>
2025-06-24 19:47:07 -0400

    added code for creating and storing the new projects

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,30 @@ function App() {
     });
 
   }
+
+  function handleAddProject(projectData){
+    setProjectsState((prevState) => {
+
+      const projectId = Math.random();
+        const newProject = {
+          ...projectData,
+          id: projectId,
+        };
+
+      return {
+        ...prevState,
+        selctedProjectId: undefined,
+         // Add the new project to the projects array
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
   
+
   let content;
 
   if(projectsState.selctedProjectId === null){
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   }else if(projectsState.selctedProjectId===undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
@@ -32,7 +51,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-   <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+   <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
    {content}
     </main>
   );
